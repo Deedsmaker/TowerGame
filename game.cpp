f21a563c544591617093f2fb896fdfd0178cbc75
Deeksmayker <Lokkins334@gmail.com>
2025-02-02 19:26:34 +0500

    test212

--- game.cpp
+++ game.cpp
@@ -1774,7 +1774,6 @@ void init_entity(Entity *entity){
         // init centipede
         // free_entity(entity);
         
-        // nocheckin
         Centipede *centipede = &entity->centipede;
         centipede->segments_ids.clear();
         // centipede->segments_ids.add(entity->id);
