281485a8ef135ef7a21af41c17b3c6866986a446
deedsmaker <Lokkins334@gmail.com>
2025-11-23 18:50:34 +0500

    logger.cpp for further improving on logging.

--- src/game.cpp
+++ src/game.cpp
@@ -146,28 +146,7 @@ inline b32 is_editor_active() {
     return editor_state == EDITOR || state_context.in_pause_editor;
 }
 
-void log_short(const char *str) {
-    Log_Message *new_log = debug.log_messages_short.append({0});
-    str_copy(new_log->data, str);
-    
-    // Printing |'s just to see that we printing something if there's much of the same things going on.
-    local_persist u64 count = 0;
-    if (count++ % 2) sprintf(new_log->data, "%s |", new_log->data);
-    else           sprintf(new_log->data, "%s |||||", new_log->data);
-    
-    new_log->birth_time = core.time.app_time;
-}
-inline void log_short(String string) {
-    log_short(c_str(string));
-}
-
-inline void log_short(f32 value) {
-    log_short(tprintf("%f", value));
-}
-inline void log_short(Vector2 value) {
-    log_short(tprintf("{%f, %f}", value.x, value.y));
-}
-
+#include "logger.cpp"
 #include "random.hpp"
 #include "particles.hpp"
 #include "old_ui.hpp"
---
+++ src/logger.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+void log_short(const char *str) {
+    Log_Message *new_log = debug.log_messages_short.append({0});
+    str_copy(new_log->data, str);
+    
+    // Printing |'s just to see that we printing something if there's much of the same things going on.
+    local_persist u64 count = 0;
+    if (count++ % 2) sprintf(new_log->data, "%s |", new_log->data);
+    else           sprintf(new_log->data, "%s |||||", new_log->data);
+    
+    new_log->birth_time = core.time.app_time;
+}
+inline void log_short(String string) {
+    log_short(c_str(string));
+}
+
+inline void log_short(f32 value) {
+    log_short(tprintf("%f", value));
+}
+inline void log_short(Vector2 value) {
+    log_short(tprintf("{%f, %f}", value.x, value.y));
+}
+
